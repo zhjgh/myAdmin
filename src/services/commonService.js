a3b2d55af5410fe523a7561811df1bd0b0337a2e
zhanghj <1058776714@qq.com>
2018-08-07 14:25:34 +0800

    bugfix

--- src/services/commonService.js
+++ src/services/commonService.js
@@ -10,6 +10,28 @@ export async function getDataService (config, params) {
   })
   const code = res.code
   //根据后端返回的code进行逻辑处理
+  if (code !== '1') {
+    if (code !== '9999') {
+      res.success = false
+      if (errorMessage[code]) {
+        message.warning(errorMessage[code], 5)
+      } else if (errorMessage[code] === null){
+
+      } else {
+        message.warning('系统错误', 5)
+      }
+    }
+  }
+  if (code === '40000') {
+		// 处理token过期，清除storage中的user对象，token
+    localStorage.removeItem('user')
+    localStorage.removeItem('token')
+    window.location = '/login'
+  } else if (code === '-1') {
+    return res
+  } else {
+    return res
+  }
 }
 
 export async function postDataService (config, params) {
@@ -20,4 +42,26 @@ export async function postDataService (config, params) {
   })
   const code = res.code
   //根据后端返回的code进行逻辑处理
+  if (code !== '1') {
+    if (code !== '9999') {
+      res.success = false
+      if (errorMessage[code]) {
+        message.warning(errorMessage[code], 5)
+      } else if (errorMessage[code] === null){
+
+      } else {
+        message.warning('系统错误', 5)
+      }
+    }
+  }
+  if (code === '40000') {
+		// 处理token过期，清除storage中的user对象，token
+    localStorage.removeItem('user')
+    localStorage.removeItem('token')
+    window.location = '/login'
+  } else if (code === '-1') {
+    return res
+  } else {
+    return res
+  }
 }
